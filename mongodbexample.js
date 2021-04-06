be8b1941fd36a084972d4a2f120999083d29db6b
Dinakaran <me@dinakaran.dev>
2021-04-06 05:31:35 +0530

    updated

--- mongodbexample.js
+++ mongodbexample.js
@@ -2,4 +2,35 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/playground')
     .then(() => console.log('connected to mongodb....'))
-    .catch(err =>console.error('error connecting mongodb....',err));
+    .catch(err => console.error('error connecting mongodb....', err));
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+});
+
+async function createCourse() {
+    const Course = mongoose.model('Course', courseSchema);
+    const course = new Course({
+        name: 'React Course',
+        author: 'Moss',
+        tags: ['react', 'frontend'],
+        isPublished: true
+    });
+    const result = await course.save();
+    console.log(result);
+}
+
+async function getCourses() {
+    const Course = mongoose.model('Course', courseSchema);
+    const courses = await Course
+    .find({author : 'Dinakaran'})
+    .select({name:1, tags: 1});
+    console.log(courses);
+}
+
+//createCourse();
+getCourses();
