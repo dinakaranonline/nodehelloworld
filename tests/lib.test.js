642e36e3d3bce8c4a8844a82de1f5d8dd20e0212
Dinakaran <me@dinakaran.dev>
2021-04-12 18:33:43 +0530

    grouping tests

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,14 +1,23 @@
 const lib = require('../lib.js');
 
-test('absolute return positive number if input is positive number',()=>{
-    const result = lib.absolute(100);
-    expect(result).toBe(100);
+describe('absolute',()=>{
+    it('should return positive number if input is positive number',()=>{
+        const result = lib.absolute(100);
+        expect(result).toBe(100);
+    });
+    
+    it('should return negative number if input is negative number',()=>{
+        const result = lib.absolute(-10);
+        expect(result).toBe(10);
+    });
+    
+    it('should return zero  if input is zer0',()=>{
+        const result = lib.absolute(0);
+        expect(result).toBe(0);
+    });
+
 });
 
-test('absolute return negative number if input is negative number',()=>{
-    const result = lib.absolute(-10);
-    expect(result).toBe(10);
-});
 
 /* test('our second test',()=>{
     throw new Error('some error happened');
